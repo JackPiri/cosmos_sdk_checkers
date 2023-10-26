bed3f9c4c58adda674d188fdc2a30c607711c862
GiacomoPirinoli <gpirinoli@horizenlabs.io>
2023-10-26 15:03:40 +0200

    Added logic for custom query

--- x/checkers/keeper/query_player_info_all_with_not_started_games.go
+++ x/checkers/keeper/query_player_info_all_with_not_started_games.go
@@ -7,6 +7,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"github.com/alice/checkers/x/checkers/rules"
 )
 
 func (k Keeper) PlayerInfoAllWithNotStartedGames(goCtx context.Context, req *types.QueryPlayerInfoAllWithNotStartedGamesRequest) (*types.QueryPlayerInfoAllWithNotStartedGamesResponse, error) {
@@ -17,7 +19,25 @@ func (k Keeper) PlayerInfoAllWithNotStartedGames(goCtx context.Context, req *typ
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Process the query
-	_ = ctx
+	var notStartedGameBoard = rules.New().String()
+	var storedGames []types.StoredGame = k.GetAllStoredGame((ctx))
+	var notStartedStoredGames []types.StoredGame
+	for _, storedGame := range storedGames {
+		if storedGame.Board == notStartedGameBoard {
+			notStartedStoredGames = append(notStartedStoredGames, storedGame)
+		}
+	}
 
-	return &types.QueryPlayerInfoAllWithNotStartedGamesResponse{}, nil
+	var playersInfo []types.PlayerInfo = k.GetAllPlayerInfo(ctx)
+	var filteredPlayersInfo []types.PlayerInfo
+	for _, playerInfo := range playersInfo {
+		for _, notStartedStoredGame := range notStartedStoredGames {
+			if playerInfo.Account == notStartedStoredGame.Black || playerInfo.Account == notStartedStoredGame.Red {
+				filteredPlayersInfo = append(filteredPlayersInfo, playerInfo)
+				break
+			}
+		}
+	}
+
+	return &types.QueryPlayerInfoAllWithNotStartedGamesResponse{PlayerInfoAllWithNotStartedGames: filteredPlayersInfo}, nil
 }
